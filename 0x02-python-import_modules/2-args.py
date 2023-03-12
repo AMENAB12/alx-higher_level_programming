67ccbb72db92d831b84ccaecb9adaac255829238
AMENAB12 <lalatube71@gmail.com>
2023-03-12 11:43:24 -0700

    done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 
+
 import sys
 
-count = len(sys.argv) - 1
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(count))
-for i in range(count):
-     print("{}: {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1  # exclude the program name itself
+    
+    if num_args == 0:
+        print("Number of arguments: 0.")
+    elif num_args == 1:
+        print("Number of argument: 1.")
+    else:
+        print(f"Number of arguments: {num_args}.")
+    
+    if num_args > 0:
+        print("Arguments:")
+        for i in range(num_args):
+            print(f"{i+1}: {sys.argv[i+1]}")
